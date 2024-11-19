3f6a0aa48d0f33481d3f3d1008e1015df98bb9b0
Felipe Pineda <fpineda.eln@gmail.com>
2024-11-18 21:03:21 -0300

    Feat: Simplify verifications (removed regex)

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,39 @@
 const button = document.getElementById('appointmentBtn');
-const nameRegex = /^[a-zA-Z]$/;
-const mailRegex =
-  /^[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9]@[a-zA-Z0-9]+\.[a-z]{2,}$/gm;
-const twoDotsRegex = /[\.]{2}/gm;
-const numRegex = /^[0-9]{9,}$/gm;
 
 button.addEventListener('click', () => {
-  let name = prompt('Ingrese su nombre y apellido:');
+  let name = prompt('Ingrese su nombre y apellido:').trim();
   if (name === null) return;
-  while (!nameRegex.test(name.trim())) {
+
+  while (name.split(' ').length < 2) {
     console.log('Invalid name:', name);
     // Se considera un nombre valido, un string que contenga al menos 2 palabras
-    console.log('Has at least 2 words:', nameRegex.test(name));
-    name = prompt('Ingrese un nombre válido:');
+    name = prompt('Ingrese un nombre válido:').trim();
   }
-  name = name.trim();
-  console.log(`name "${name}" was valid`);
+  console.log(`name "${name}" is valid`);
 
-  let email = prompt('Ingrese su correo:');
+  let email = prompt('Ingrese su correo:').trim();
   if (email === null) {
     name = null;
   }
-  while (!mailRegex.test(email) || twoDotsRegex.test(email)) {
-    // El formato valido de email es <usuario>@<dominio>.<ext>
-    // usuario: Permite letras, numeros, puntos y guiones
-    // dominio: Permite solo letras y numeros
-    // ext: La extensión permite solo letras y debe contener al menos 2 caracteres
-    // en ningun caso pueden haber 2 puntos seguidos.
+  while (!email.includes('@') || !email.includes('.')) {
+    // Se considera como email valido cualquier string que contenga un caracter @
+    // y un caracter .
     console.log('Invalid email:', email);
-    console.log('Has email format:', mailRegex.test(email.trim));
-    console.log(
-      "Doesn't have two consecutive dots '..':",
-      !twoDotsRegex.test(email)
-    );
-    email = prompt('Ingrese un correo válido:');
+    email = prompt('Ingrese un correo válido:').trim();
   }
-  email = email.trim();
   console.log(`email "${email}" was valid`);
 
-  let phone = prompt('Ingrese un número de teléfono');
+  let phone = prompt('Ingrese un número de teléfono').trim();
   if (phone === null) {
     name = null;
     email = null;
     return;
   }
-  while (!numRegex.test(phone)) {
+  while (isNaN(Number(phone))) {
     // Se considera un numero telefonico valido cualquier string
-    // que contenga solo números y contenga al menos 9 caracteres
+    // que se pueda convertir a número
     console.log('Invalid phone:', phone);
-    phone = prompt('Ingrese un teléfono válido:');
+    phone = prompt('Ingrese un teléfono válido:').trim();
   }
   console.log('Datos ingresados: ');
   console.log('Nombre:', name);
